1d1ee5d8ecff39316f12fdea70bae3a478ef42cc
Navaneethakrishnan <krish.nava007@gmail.com>
2021-08-16 20:37:16 +0530

    :zap: ip added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://10.0.0.105/'
+axios.defaults.baseURL = 'http://10.0.3.75/'
 
 
 ReactDOM.render(
